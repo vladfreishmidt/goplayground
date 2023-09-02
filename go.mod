c8fcc443c6cbfb78859050a021eaa64761951fc3
vladfreishmidt <vladfreishmidt.webdevelopment@gmail.com>
2023-09-02 10:54:03 +0300

    Add packages

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/vladfreishmidt/goplayground
+
+go 1.21.0
+
+require github.com/vladfreishmidt/puppy v0.0.0-20230901125938-892ceb0c81c9
+
+require github.com/vladfreishmidt/dog v0.0.0-20230901113912-7cb42dceaf10 // indirect
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/vladfreishmidt/puppy"
+)
+
+func main() {
+	fmt.Println("Hello, Gophers! 😊")
+	fmt.Println(puppy.Bark())
+	fmt.Println(puppy.Barks())
+
+	s1 := puppy.BigBark()
+	s2 := puppy.BigBarks()
+
+	fmt.Println(s1)
+	fmt.Println(s2)
+
+}
